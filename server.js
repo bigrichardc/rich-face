d819bc7f4994f8b3cb31cbe854011ce2cc0cb176
bigrichardc <st00ka@yahoo.com>
2020-10-04 12:16:42 +0100

    Testing heroku postgres
    Trying a database

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
@@ -11,6 +20,21 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
+
+app.get('/dbtest', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('Select * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } 
+  catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
